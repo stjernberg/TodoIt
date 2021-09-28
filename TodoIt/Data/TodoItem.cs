e1b9d14b1b79ec93eeab23a5d4f1f55048fe44d5
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-09-28 20:23:47 +0200

    Almost don with adding methods and test to TodoItem styep 10.

--- TodoIt/Data/TodoItem.cs
+++ TodoIt/Data/TodoItem.cs
@@ -49,8 +49,8 @@ namespace TodoIt.Data
 
         public Todo CreateNewTodo(string description)
         {
-            Todo newTodo = new Todo(TodoSequencer.NextTodoId(), description); 
-            Array.Resize(ref todoArray, todoArray.Length + 1);            
+            Todo newTodo = new Todo(TodoSequencer.NextTodoId(), description);
+            Array.Resize(ref todoArray, todoArray.Length + 1);
             todoArray[todoArray.Length - 1] = newTodo;
             return newTodo;
         }
@@ -60,5 +60,66 @@ namespace TodoIt.Data
             todoArray = new Todo[0];
         }
 
-    }
-}
+        public Todo[] FindByDoneStaus(bool doneStatus)
+        {
+            Todo[] filteredArray = new Todo[0];
+            foreach (Todo todoItem in todoArray)
+            {
+                if (todoItem.Done == doneStatus)
+                {
+                    Array.Resize(ref filteredArray, filteredArray.Length + 1);
+                    filteredArray[filteredArray.Length - 1] = todoItem;
+                }
+            }
+            return filteredArray;
+        }
+        public Todo[] FindByAssignee(int personId)
+        {
+            Todo[] filteredArray = new Todo[0];
+            foreach (Todo todoItem in todoArray)
+            {
+                if (todoItem.Assignee != null)
+                {
+                    if (todoItem.Assignee.PersonId == personId)
+                    {
+                        Array.Resize(ref filteredArray, filteredArray.Length + 1);
+                        filteredArray[filteredArray.Length - 1] = todoItem;
+                    }
+                }
+
+            }
+            return filteredArray;
+        }
+
+        public Todo[] FindByAssignee(Person assignee)
+        {
+            Todo[] filteredArray = new Todo[0];
+
+            foreach (Todo todoItem in todoArray)
+            {
+                if (todoItem.Assignee == assignee)
+                {
+                    Array.Resize(ref filteredArray, filteredArray.Length + 1);
+                    filteredArray[filteredArray.Length - 1] = todoItem;
+                }
+            }
+            return filteredArray;
+        }
+
+        public Todo[] FindUnassignedTodoItems()
+        {
+            Todo[] filteredArray = new Todo[0];
+            foreach (Todo todoItem in todoArray)
+            {
+                if (todoItem.Assignee == null)
+                {
+                    Array.Resize(ref filteredArray, filteredArray.Length + 1);
+                    filteredArray[filteredArray.Length - 1] = todoItem;
+                }
+            }
+            return filteredArray;
+        }
+
+    }//class
+}//namespace
+
